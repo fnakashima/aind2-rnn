1ba9e49544056205efe9a160dbf69da34a4d5fa2
fnakashima <fuminaka387@gmail.com>
2017-10-24 08:59:05 +0100

    finished till 2.2

--- my_answers.py
+++ my_answers.py
@@ -4,14 +4,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
+import string
 
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
+    #print(series)
     # containers for input/output pairs
     X = []
     y = []
+    length = len(series)-window_size
+    for i in range(length):
+        X.append(series[i:i + window_size])
+        y.append(series[i + window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -19,16 +25,29 @@ def window_transform_series(series, window_size):
     y = np.asarray(y)
     y.shape = (len(y),1)
 
+    #print("X\n",X)
+    #print("y\n",y)
+
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    return model
     pass
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    valid_chars = list(string.ascii_lowercase)
+    valid_chars.append(punctuation)
+    for c in text:
+        if c not in valid_chars:
+            text = text.replace(c,' ')
 
     return text
 
